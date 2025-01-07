13ad081fab50e498f0e8f3220d93aa2b35274944
Anna Oleynikova <anitaulitka@gmail.com>
2025-01-07 15:39:52 +1000

    pep8

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-!h&t*k(d*a&!npvwq47t)t2=netd3=q(pz%cf=96=^ej_$9d6i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
